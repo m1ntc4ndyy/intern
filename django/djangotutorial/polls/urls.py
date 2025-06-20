d737a32f5d7a9eab8976b0795eebf8d258f4e6f4
m1ntc4ndyy <123210141@sv1.dut.udn.vn>
2025-06-20 15:30:41 +0700

    using template for views

--- django/djangotutorial/polls/urls.py
+++ django/djangotutorial/polls/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),    
+    # /polls/
+    path('', views.index, name='index'),
+    # /polls/<question_id>/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # /polls/<question_id>/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- django/djangotutorial/polls/views.py
+++ django/djangotutorial/polls/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("You are looking at the index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    print(latest_question_list)
+    context  = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
+    
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}.")
