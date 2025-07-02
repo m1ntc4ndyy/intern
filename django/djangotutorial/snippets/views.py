b492c84257eb80a549304864cad1eaff6eeaeadf
m1ntc4ndyy <123210141@sv1.dut.udn.vn>
2025-07-02 14:05:48 +0700

    part 5 final

--- django/djangotutorial/snippets/views.py
+++ django/djangotutorial/snippets/views.py
@@ -1,10 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics, renderers
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import isOwnerOrReadOnly
 from django.contrib.auth.models import User
 # Create your views here.
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -15,6 +25,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
     
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
