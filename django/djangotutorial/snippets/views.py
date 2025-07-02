a1ba6380e95e53de5cb3bf47995b8e1f7cb64326
m1ntc4ndyy <123210141@sv1.dut.udn.vn>
2025-07-02 15:47:57 +0700

    tutorial part 6

--- django/djangotutorial/snippets/views.py
+++ django/djangotutorial/snippets/views.py
@@ -1,6 +1,6 @@
-from rest_framework import generics, renderers
+from rest_framework import generics, renderers, viewsets, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from snippets.models import Snippet
@@ -15,27 +15,21 @@ def api_root(request, format=None):
         'users': reverse('user-list', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format),
     })
-class SnippetList(generics.ListCreateAPIView):
+
+class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, isOwnerOrReadOnly]
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-    def get(self, request, *args, **kwargs):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          isOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
-    
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-class UserDetail(generics.RetrieveAPIView):
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
